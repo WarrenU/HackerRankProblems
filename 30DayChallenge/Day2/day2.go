1df2cfb5e185c0cb61942f50a0605ee7428e192e
Warren Urbina <warren.urbina@gmail.com>
2017-09-12 22:25:25 -0700

    day 2 challenge

---
+++ 30DayChallenge/Day2/day2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strconv"
+  "math"
+)
+
+func round(num float64) int {
+    return int(num + math.Copysign(0.5, num))
+}
+
+func main(){
+    scanner := bufio.NewScanner(os.Stdin)
+
+    var meal_cost float64
+    var tip_percentage uint64
+    var tax_percentage uint64
+    var total_cost float64
+
+    value := make([]string, 3)
+    var count int = 0
+    for scanner.Scan() {
+      value[count] = scanner.Text()
+      count++
+    }
+
+    meal_cost, _ = strconv.ParseFloat(value[0], 64)
+    tip_percentage, _ = strconv.ParseUint(value[1], 10, 64)
+    tax_percentage, _ = strconv.ParseUint(value[2], 10, 64)
+
+    var tip float64 = meal_cost * (float64(tip_percentage)/100.0)
+    //fmt.Printf("%v\n", tip)
+    var tax float64 = meal_cost * (float64(tax_percentage)/100.0)
+    //fmt.Printf("%v\n", tax)
+    total_cost = meal_cost + tip + tax
+
+    fmt.Print("The total meal cost is ")
+    fmt.Printf("%d", round(total_cost))
+    fmt.Print(" dollars.")
+}
