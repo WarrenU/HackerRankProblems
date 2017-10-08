fa746359b5a4a3779e48c50f2807132d247497a6
Warren Urbina <warren.urbina@gmail.com>
2017-10-08 09:43:50 -0700

    day14

---
+++ 30DayChallenge/day14.py
@@ -0,0 +1,12 @@
+
+class Difference:
+    def __init__(self, a):
+        self.__elements = ada
+        # Add your code here
+
+    def get_elements(self):
+        return self.__elements
+
+    def computeDifference(self):
+        nums = self.get_elements()
+        self.maximumDifference = max(nums) - min(nums)
